b7e8b8f7a7836e58a72320a60812818ad1e4df16
ST10191639-Pearl-Mashaba <st10191639@vcconnect.edu.za>
2025-04-03 11:56:07 +0200

    Redid the Eventease application to fix error

---
+++ EEApp/Data/EEDBContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using EEApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EEApp.Data;
+
+public partial class EEDBContext : DbContext
+{
+    public EEDBContext()
+    {
+    }
+
+    public EEDBContext(DbContextOptions<EEDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Venue> Venues { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Booking>(entity =>
+        {
+            entity.HasKey(e => e.BookingId).HasName("PK__Booking__73951AED748DCE9D");
+
+            entity.ToTable("Booking");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.EventId)
+                .HasConstraintName("FK__Booking__EventId__3B75D760");
+
+            entity.HasOne(d => d.Venue).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.VenueId)
+                .HasConstraintName("FK__Booking__VenueId__3C69FB99");
+        });
+
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.EventId).HasName("PK__Event__7944C810581714B9");
+
+            entity.ToTable("Event");
+
+            entity.Property(e => e.Description)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.EventName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Venue>(entity =>
+        {
+            entity.HasKey(e => e.VenueId).HasName("PK__Venue__3C57E5F2497ABED5");
+
+            entity.ToTable("Venue");
+
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Location)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.VenueName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EEApp/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EEApp.Models;
+
+public partial class Booking
+{
+    public int BookingId { get; set; }
+
+    public int? EventId { get; set; }
+
+    public int? VenueId { get; set; }
+
+    public DateOnly BookingDate { get; set; }
+
+    public virtual Event? Event { get; set; }
+
+    public virtual Venue? Venue { get; set; }
+}
